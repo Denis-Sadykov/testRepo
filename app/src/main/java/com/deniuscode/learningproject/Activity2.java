866c38b3fe903ff6045a5d8761147768800405ca
DeniSadykov <video.kr4.123@gmail.com>
2020-08-22 12:00:30 +0500

    add new files

--- app/src/main/java/com/deniuscode/learningproject/Activity2.java
+++ app/src/main/java/com/deniuscode/learningproject/Activity2.java
@@ -2,13 +2,27 @@ package com.deniuscode.learningproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Activity2 extends AppCompatActivity {
+public class Activity2 extends AppCompatActivity implements View.OnClickListener {
+
+    public Button buttonChangeActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.activity_main);
+
+        buttonChangeActivity = findViewById(R.id.button_activity_02);
+        buttonChangeActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
