0ecfaad88276a9b2f57588bac3276d51c16db0ed
DeniSadykov <video.kr4.123@gmail.com>
2020-08-22 12:03:29 +0500

    fix bug in Activity2.java

--- app/src/main/java/com/deniuscode/learningproject/Activity2.java
+++ app/src/main/java/com/deniuscode/learningproject/Activity2.java
@@ -14,7 +14,7 @@ public class Activity2 extends AppCompatActivity implements View.OnClickListener
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_2);
 
         buttonChangeActivity = findViewById(R.id.button_activity_02);
         buttonChangeActivity.setOnClickListener(this);
